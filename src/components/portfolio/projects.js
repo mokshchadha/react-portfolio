b79fb06ad08420b14657cf4e391de96ad151d61c
moksh chadha <mokshchadha@mokshs-MacBook-Pro.local>
2023-06-20 11:47:03 +0530

    added some meta tags for testing

--- src/components/portfolio/projects.js
+++ src/components/portfolio/projects.js
@@ -1,6 +1,4 @@
-import IMG1 from "../../assets/prod1.jpeg";
 import IMG2 from "../../assets/p2.gif";
-import IMG3 from "../../assets/p3.gif";
 import IMG4 from "../../assets/prod4.jpeg";
 import IMG0 from "../../assets/prod0.png";
 import IMG5 from "../../assets/p5.gif";
@@ -20,13 +18,13 @@ const PROJECTS = [
     liveDemoLink: "https://covid-wb.herokuapp.com/",
     imgSrc: IMG0,
   },
-  {
-    key: 1,
-    title: "Travel Advisor",
-    githubLink: "https://github.com/mokshchadha/travel-advisor",
-    liveDemoLink: "https://loving-thompson-d3964d.netlify.app/",
-    imgSrc: IMG1,
-  },
+  // {
+  //   key: 1,
+  //   title: "Travel Advisor",
+  //   githubLink: "https://github.com/mokshchadha/travel-advisor",
+  //   liveDemoLink: "https://loving-thompson-d3964d.netlify.app/",
+  //   imgSrc: IMG1,
+  // },
   {
     key: 2,
     title: "Sorting Visualizer",
@@ -34,13 +32,13 @@ const PROJECTS = [
     liveDemoLink: "https://gallant-lovelace-64dc8c.netlify.app/",
     imgSrc: IMG2,
   },
-  {
-    key: 3,
-    title: "Matrix Rain",
-    githubLink: "https://github.com/mokshchadha/matrix-rain",
-    liveDemoLink: "https://epic-fermi-50d3fb.netlify.app/",
-    imgSrc: IMG3,
-  },
+  // {
+  //   key: 3,
+  //   title: "Matrix Rain",
+  //   githubLink: "https://github.com/mokshchadha/matrix-rain",
+  //   liveDemoLink: "https://epic-fermi-50d3fb.netlify.app/",
+  //   imgSrc: IMG3,
+  // },
   {
     key: 4,
     title: "Learn Hooks In Depth",
