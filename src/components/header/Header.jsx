e3f067a432f067a3122a4a32a5a211821e587bbc
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2024-12-08 19:53:01 +0530

    header image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import "./header.css";
+import IMG from "../../assets/image_header.jpeg"
 
 import CTA from "./CTA";
 import HeaderSocials from "./HeaderSocials";
 
 export const Header = () => {
   return (
-    <div className="container header__container" style={{ zIndex: 500 }}>
-      <h5>Hello I am</h5>
-      <h1 className="text-4xl">Moksh Rai Chadha</h1>
-      <h5 className="text-light text-sm">Software Developer</h5>
-      <CTA />
-      <HeaderSocials />
-      <a  className="text-blue-500 hover:text-blue-700 scroll__down" href="#footer"  >
-        Scroll Down
-      </a>
-    </div>
+    <header className="header">
+      <div className="container header__container" style={{ zIndex: 500 }}>
+        <div className="header__content">
+          <div className="header__text">
+            <h5>Hello I am</h5>
+            <h1 className="text-4xl">Moksh Rai Chadha</h1>
+            <h5 className="text-light text-sm">Software Developer</h5>
+            <CTA />
+            <HeaderSocials />
+            <a className="text-blue-500 hover:text-blue-700 scroll__down" href="#footer">
+              Scroll Down
+            </a>
+          </div>
+          <div className="header__image">
+            <img src={IMG} alt="Moksh Rai Chadha" className="header__profile" />
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
